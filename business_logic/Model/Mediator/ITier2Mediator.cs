662a179becd71149cb26c67d3cf70eb9edbbef7b
lukisko <lukas.pleva@mail.com>
2021-10-27 10:15:44 +0200

    add tier2 handler interface methods

--- business_logic/Model/Mediator/ITier2Mediator.cs
+++ business_logic/Model/Mediator/ITier2Mediator.cs
@@ -1,7 +1,13 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace business_logic.Model.Mediator
 {
     public interface ITier2Mediator
     {
-         
+        Task<PetList> requestPets();
+        Task<Pet> createPet(Pet newPet);
     }
+
+    
 }
