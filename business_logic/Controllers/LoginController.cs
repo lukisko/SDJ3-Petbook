0e29af680471c1c1339b7a77ca1988af6bd68d0e
lukisko <lukas.pleva@mail.com>
2021-11-22 09:30:03 +0100

    add new files

--- business_logic/Controllers/LoginController.cs
+++ business_logic/Controllers/LoginController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using business_logic.Model;
+using System.Net.Http;
 
 namespace business_logic.Controllers
 {
@@ -16,6 +17,7 @@ namespace business_logic.Controllers
 
         public  UserController(IModel model){
             this.model = model;
+            
         }
 
         [HttpGet]
@@ -29,7 +31,6 @@ namespace business_logic.Controllers
             }catch (Exception e){
                 return StatusCode(400,"the login was not successful");
             }
-            
         }
 
         [HttpPost]
