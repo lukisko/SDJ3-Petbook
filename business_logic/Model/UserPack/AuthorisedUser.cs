995c4b5566b9ceec779307d0f432d6259920169d
lukisko <lukas.pleva@mail.com>
2021-11-24 16:42:43 +0100

    try to make login work

---
+++ business_logic/Model/UserPack/AuthorisedUser.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace business_logic.Model.UserPack
+{
+    public class AuthorisedUser: User
+    {
+        
+        public Pet[] pets {get;set;}
+    }
+}
