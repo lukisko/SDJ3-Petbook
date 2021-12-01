8119d7fbd665a5c40989c02e35771b17ba26c2f8
nickcernean <nicolaecer@gmail.com>
2021-12-01 14:13:40 +0100

    login_register_working

--- ClientApp/Pages/Login.razor.cs
+++ ClientApp/Pages/Login.razor.cs
@@ -4,30 +4,54 @@ using Microsoft.AspNetCore.Components;
 
 namespace ClientApp.Pages
 {
-    public partial class Login :ComponentBase
+    public partial class Login : ComponentBase
     {
         [Parameter] public string Email { get; set; }
         private string _confirmationCode;
         private string _errorMessage;
+        private bool ShowPopUpDialog;
+
         protected override async Task OnInitializedAsync()
         {
+            if (String.IsNullOrEmpty(Email))
+            {
+                ShowPopUpDialog = true;
+            }
             _errorMessage = "";
             _confirmationCode = null;
         }
-    
+
         private async void LoginUser()
         {
-            Console.WriteLine(Email);
-            // try
-            // {
-            //   await _userController.Login(Email, _confirmationCode);
-            //     Email = null;
-            //     _confirmationCode = null;
-            // }
-            // catch (Exception e)
-            // {
-            //     _errorMessage = e.Message;
-            // }
+            try
+            {
+                await _userController.Login(Email, _confirmationCode);
+                Email = null;
+                _confirmationCode = null;
+                NavMgr.NavigateTo("/");
+            }
+            catch (Exception e)
+            {
+                _errorMessage = e.Message;
+            }
+        }
+
+        private  void SendCode(string email)
+        {
+            try
+            {   // maybe awaitable in the future
+                 _userController.SendEmail(email);
+                ShowPopUpDialog = false;
+            }
+            catch (Exception e)
+            {
+                _errorMessage = e.Message;
+            }
+        }
+
+        public void NavigateToRegister()
+        {
+            NavMgr.NavigateTo("/Register");
         }
     }
 }
--- ClientApp/obj/Debug/net5.0/Razor/Shared/SurveyPrompt.razor.g.cs
+++ ClientApp/obj/Debug/net5.0/Razor/Shared/SurveyPrompt.razor.g.cs
@@ -94,7 +94,7 @@ using ClientApp.Data;
             __builder.OpenElement(0, "div");
             __builder.AddAttribute(1, "class", "alert alert-secondary mt-4");
             __builder.AddAttribute(2, "role", "alert");
-            __builder.AddMarkupContent(3, "<span class=\"oi oi-pencil mr-2\" aria-hidden=\"true\"></span>\n    ");
+            __builder.AddMarkupContent(3, "<span class=\"oi oi-pencil mr-2\" aria-hidden=\"true\"></span>\r\n    ");
             __builder.OpenElement(4, "strong");
             __builder.AddContent(5, 
 #nullable restore
@@ -106,8 +106,8 @@ using ClientApp.Data;
 #nullable disable
             );
             __builder.CloseElement();
-            __builder.AddMarkupContent(6, "\n\n    ");
-            __builder.AddMarkupContent(7, "<span class=\"text-nowrap\">\n        Please take our\n        <a target=\"_blank\" class=\"font-weight-bold\" href=\"https://go.microsoft.com/fwlink/?linkid=2137813\">brief survey</a></span>\n    and tell us what you think.\n");
+            __builder.AddMarkupContent(6, "\r\n\r\n    ");
+            __builder.AddMarkupContent(7, "<span class=\"text-nowrap\">\r\n        Please take our\r\n        <a target=\"_blank\" class=\"font-weight-bold\" href=\"https://go.microsoft.com/fwlink/?linkid=2137813\">brief survey</a></span>\r\n    and tell us what you think.\r\n");
             __builder.CloseElement();
         }
         #pragma warning restore 1998
