09eda270aff3039eca7d6a7fb3a1abb5c692ef91
hanchry <jurco10510@gmail.com>
2021-12-15 14:49:17 +0100

    initializing database connection before sockets

--- database/src/main/java/Main.java
+++ database/src/main/java/Main.java
@@ -1,9 +1,11 @@
+import DatabasePersistence.Database;
 import mediator.T3Server;
 
 public class Main
 {
   public static void main(String[] args)
   {
+    Database database = Database.getInstance();
     T3Server server = new T3Server();
     server.run();
   }
